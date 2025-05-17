3a8b99587d059f27730f23843e5bcdbb8b368a84
Rapunzel <osina_kv@mail.ru>
2025-05-17 18:09:17 +0300

    small fixes do_post, add nul favicon.ico

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ import os
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/" or self.path == "/contacts.html":
-            self.respond_with_file("contacts.html")
-        else:
-            self.send_error(404, "Файл не найден")
+        if self.path == '/':
+            self.path = '/index.html'
+
+        if '..' in self.path:
+            raise FileNotFoundError()
+
+        with open(self.path.lstrip('/'), 'rb') as f:
+            content = f.read()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(content)
 
     def do_POST(self):
         content_length = int(self.headers.get('Content-Length', 0))
@@ -22,17 +30,17 @@ class SimpleHandler(BaseHTTPRequestHandler):
 
         self.respond_with_file("contacts.html")
 
-    def respond_with_file(self, filename):
-        try:
-            with open(filename, "rb") as f:
-                content = f.read()
-            self.send_response(200)
-            content_type = self.guess_content_type(filename)
-            self.send_header("Content-type", content_type)
-            self.end_headers()
-            self.wfile.write(content)
-        except FileNotFoundError:
-            self.send_error(404, f"Файл {filename} не найден")
+    # def respond_with_file(self, filename):
+    #     try:
+    #         with open(filename, "rb") as f:
+    #             content = f.read()
+    #         self.send_response(200)
+    #         content_type = self.guess_content_type(filename)
+    #         self.send_header("Content-type", content_type)
+    #         self.end_headers()
+    #         self.wfile.write(content)
+    #     except FileNotFoundError:
+    #         self.send_error(404, f"Файл {filename} не найден")
 
 
 if __name__ == "__main__":
